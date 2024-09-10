6735d2cb64413f52528e58a54e4ee2a87dd84400
EvgeniyaAlekseevna <zhenya.sudniczina@yandex.ru>
2024-09-11 00:48:11 +0300

    Задача 1 Доработано выделение записей, теперь при выделении любой записи сбрасывается выделения других записей. Клик по выделенной записи тоже отменяет выделение. Задача 2 Доработано создание новой записи. Код записи не повторяется при создании/удалении. Задача 3 Выведено количество совершенных выделений для каждого пункта фразой “Выделяли N раз”. По умолчанию у всех ноль. Фраза с нулём не выводится.

--- src/store.js
+++ src/store.js
@@ -3,8 +3,13 @@
  */
 class Store {
   constructor(initState = {}) {
-    this.state = initState;
+    this.state = {
+      ...initState,
+      lastCode: 0
+    };
     this.listeners = []; // Слушатели изменений состояния
+    this.setHighlightedCount()
+    this.getAvailableCode()
   }
 
   /**
@@ -42,9 +47,19 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    const newCode = this.state.lastCode + 1;
+    const newItem = {
+      code: newCode,
+      title: `Новая запись_${newCode}`,
+      highlightedCount: 0
+    }
+
     this.setState({
       ...this.state,
-      list: [...this.state.list, { code: this.state.list.length + 1, title: 'Новая запись' }],
+      list: [
+        ...this.state.list,
+        newItem],
+      lastCode: newCode
     });
   }
 
@@ -64,15 +79,75 @@ class Store {
    * @param code
    */
   selectItem(code) {
+    const updatedList = this.state.list.map(item => {
+      const isSelected = item.code === code;
+      return ({
+        ...item,
+        selected: isSelected ? !item.selected : false,
+        highlightedCount: item.highlightedCount + (isSelected && !item.selected === true ? 1 : 0)
+      });
+    })
+
     this.setState({
       ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      }),
+      list: updatedList
     });
+  }
+
+  /**
+   * Получение составного имени элемента
+   * @param code
+   * @returns {string}
+   */
+  getTitleItem(code){
+    let item = this.state.list.find(item => item.code === code);
+    if (item.highlightedCount === 0)
+      return `${item.title}`
+    return `${item.title} | Выделяли ${this.getCorrectWordForm(item.highlightedCount)}`;
+  }
+
+  /**
+   * Вывод корректно формы слова "раз/раза"
+   * @param n
+   * @returns {string}
+   */
+  getCorrectWordForm(n){
+    if ((n % 100 >= 11 && n % 100 <= 14) || (n % 10 === 1))
+      return `${n} раз`;
+    else if (n % 10 >= 2 && n % 10 <= 4)
+      return `${n} раза`;
+    else
+      return `${n} раз`;
+  };
+
+  /**
+   * Установка начального значения количества выделений записи
+   */
+  setHighlightedCount(){
+    if (Array.isArray(this.state.list)) {
+      this.state.list.forEach(item => {
+        item.highlightedCount = 0;
+      });
+    }
+  }
+
+  /**
+   * Получение использованного кода записи
+   */
+  getAvailableCode(){
+    let maxCode = 0;
+    if (!Array.isArray(this.state.list) || this.state.list.length === 0)
+      maxCode = 0;
+    else
+      maxCode = this.state.list.reduce((max, current) =>
+          Math.max(max, current.code),
+        0
+      );
+
+    this.setState({
+      ...this.state,
+      lastCode: maxCode
+    })
   }
 }
 
